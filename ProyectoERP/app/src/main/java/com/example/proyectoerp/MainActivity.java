f96bd79a75bdbea8852b8f873e304d4df83a0542
Xandre18 <byxass18@gmail.com>
2023-03-01 13:07:21 +0100

    Funciona el navigationMenu

--- ProyectoERP/app/src/main/java/com/example/proyectoerp/MainActivity.java
+++ ProyectoERP/app/src/main/java/com/example/proyectoerp/MainActivity.java
@@ -1,23 +1,63 @@
 package com.example.proyectoerp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
-    TextView tv;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv = findViewById(R.id.textView);
-        Bundle b = getIntent().getExtras();
 
+        Toolbar toolbar = findViewById(R.id.toolbar); //Ignore red line errors
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.open_nav,R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+
+        Bundle b = getIntent().getExtras();
         if(b.getBoolean("isAdmin")){
-            tv.setText("Admin");
+
         }else{
-            tv.setText("User");
+
         }
+    }
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+                break;
+            case R.id.nav_tesoreria:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new TesoreriaFragment()).commit();
+                break;
+            case R.id.nav_clientes:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ClientesFragment()).commit();
+                break;
+            case R.id.nav_proveedores:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new ProveedoresFragment()).commit();
+                break;
+            case R.id.nav_correo:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CorreoFragment()).commit();
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
